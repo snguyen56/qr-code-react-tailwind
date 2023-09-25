df350d412b5a75baa0cda5475bbca9e1a976d3aa
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2023-09-25 17:55:03 -0400

    moved attribution

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,32 @@ import qrCode from "./assets/images/image-qr-code.png";
 function App() {
   return (
     <div className="bg-light-gray h-screen grid place-items-center">
-      <div className="bg-white w-[320px] h-[496px] rounded-[20px] p-4 text-center flex flex-col items-center gap-4 shadow-sm">
+      <div className="bg-white w-[320px] h-[496px] rounded-[20px] p-4 text-center flex flex-col items-center gap-4 shadow-sm relative">
         <div className="w-[288px] mb-2">
-          <img src={qrCode} alt="qr code" className="rounded-xl" />
+          <img src={qrCode} alt="qr code" className="rounded-[10px]" />
         </div>
-        <h1 className=" text-dark-blue font-bold text-xl px-3 text-[22px]">
+        <h1 className=" text-dark-blue font-bold text-xl px-3 text-[22px] ">
           Improve your front-end skills by building projects
         </h1>
         <p className="text-grayish-blue text-[15px] px-3 leading-tight tracking-[0.01em]">
           Scan the QR code to visit Frontend Mentor and take your coding skills
           to the next level
         </p>
-      </div>
-      <div className="text-center text-[11px] absolute bottom-0">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by{" "}
-        <a href="#" className="text-attribution">
-          Steven Nguyen
-        </a>
-        .
+        <div className="text-center text-[11px] absolute -bottom-8">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            className="text-attribution"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a href="#" className="text-attribution">
+            Steven Nguyen
+          </a>
+          .
+        </div>
       </div>
     </div>
   );
