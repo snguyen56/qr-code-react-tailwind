667da9ee5fbcfab3b77daa36f88abb4c21d993e8
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2023-09-26 16:20:10 -0400

    minor tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ function App() {
     <div className="bg-light-gray h-screen grid place-items-center">
       <div className="bg-white w-[320px] h-[496px] rounded-[20px] p-4 text-center flex flex-col items-center gap-4 shadow-sm relative">
         <div className="w-[288px] mb-2">
-          <img src={qrCode} alt="qr code" className="rounded-[10px]" />
+          <img
+            src={qrCode}
+            alt="Frontend Mentor QR code"
+            className="rounded-[10px]"
+          />
         </div>
-        <h1 className=" text-dark-blue font-bold text-xl px-3 text-[22px] ">
+        <h1 className="text-dark-blue font-bold text-xl px-3 text-[22.2px]">
           Improve your front-end skills by building projects
         </h1>
         <p className="text-grayish-blue text-[15px] px-3 leading-tight tracking-[0.01em]">
@@ -24,7 +28,11 @@ function App() {
             Frontend Mentor
           </a>
           . Coded by{" "}
-          <a href="#" className="text-attribution">
+          <a
+            href="https://github.com/snguyen56"
+            target="_blank"
+            className="text-attribution"
+          >
             Steven Nguyen
           </a>
           .
