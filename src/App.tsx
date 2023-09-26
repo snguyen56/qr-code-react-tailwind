80c17a53b40e69a0107038b945f33c0ff7aa26ab
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2023-09-26 16:42:34 -0400

    add prettier tailwind formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ import qrCode from "./assets/images/image-qr-code.png";
 
 function App() {
   return (
-    <div className="bg-light-gray h-screen grid place-items-center">
-      <div className="bg-white w-[320px] h-[496px] rounded-[20px] p-4 text-center flex flex-col items-center gap-4 shadow-sm relative">
-        <div className="w-[288px] mb-2">
+    <div className="grid h-screen place-items-center bg-light-gray">
+      <div className="relative flex h-[496px] w-[320px] flex-col items-center gap-4 rounded-[20px] bg-white p-4 text-center shadow-sm">
+        <div className="mb-2 w-[288px]">
           <img
             src={qrCode}
             alt="Frontend Mentor QR code"
             className="rounded-[10px]"
           />
         </div>
-        <h1 className="text-dark-blue font-bold text-xl px-3 text-[22.2px]">
+        <h1 className="px-3 text-[22px] text-xl font-bold text-dark-blue">
           Improve your front-end skills by building projects
         </h1>
-        <p className="text-grayish-blue text-[15px] px-3 leading-tight tracking-[0.01em]">
+        <p className="px-3 text-[15px] leading-tight tracking-[0.01em] text-grayish-blue">
           Scan the QR code to visit Frontend Mentor and take your coding skills
           to the next level
         </p>
-        <div className="text-center text-[11px] absolute -bottom-8">
+        <div className="absolute -bottom-8 text-center text-[11px]">
           Challenge by{" "}
           <a
             href="https://www.frontendmentor.io?ref=challenge"
